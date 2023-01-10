91a11ac95b0d30afb9cc3064c82c41abc7fc857c
turtle601 <turtle601@naver.com>
2023-01-10 22:08:45 +0900

    test: 프로미스 체이닝 동기, 비동기 테스트 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -6,22 +6,17 @@ function myPromiseFn() {
   });
 }
 
-describe('MyPromise 테스트 동작 테스트 확인', () => {
-  test('(단위) 프로미스 체이닝이 제대로 실행되는지', () => {
-    jest.useFakeTimers();
-    console.log = jest.fn();
-
-    myPromiseFn()
-      .then((result) => console.log(result))
-      .catch((result) => console.log(result));
-
-    setTimeout(() => {
-      expect(console.log).toHaveBeenCalledTimes(1);
-    }, 0);
-
-    jest.runAllTimers();
+function myPromiseFn2(input) {
+  return new MyPromise((resolve, reject) => {
+    if (input === 1) {
+      resolve('성공');
+    } else {
+      reject('실패');
+    }
   });
+}
 
+describe('MyPromise 테스트 동작 테스트 확인', () => {
   test('(종합) Promise가 우선순위에 의해 제대로 실행되는지', () => {
     const testLogic = () => {
       console.log('콜스택 실행 - 1');
@@ -52,6 +47,49 @@ describe('MyPromise 테스트 동작 테스트 확인', () => {
         expect(console.log).toHaveBeenCalledWith(result);
       });
     }, 1);
+
+    jest.runAllTimers();
+  });
+
+  test('(종합) 프로미스 체이닝이 제대로 실행되는지 - 동기로직', () => {
+    jest.useFakeTimers();
+    console.log = jest.fn();
+
+    new MyPromise((resolve, reject) => {
+      resolve('첫번째 프로미스');
+    }).then((value) => {
+      console.log(value); //  '첫번째 프로미스'
+      return '두번째 프로미스';
+    });
+
+    setTimeout(() => {
+      expect(console.log).toHaveBeenCalledWith('첫번째 프로미스');
+    }, 0);
+
+    jest.runAllTimers();
+  });
+
+  test('(종합) 프로미스 체이닝이 제대로 실행되는지 - 비동기로직', () => {
+    jest.useFakeTimers();
+    console.log = jest.fn();
+
+    new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('첫번째 프로미스');
+      }, 1000);
+    })
+      .then((value) => {
+        console.log(value); // `첫번째 프로미스`
+        return '두번째 프로미스';
+      })
+      .then((value) => {
+        console.log(value); // `두번째 프로미스`
+      });
+
+    setTimeout(() => {
+      expect(console.log).toHaveBeenCalledWith('첫번째 프로미스');
+      expect(console.log).toHaveBeenCalledWith('두번째 프로미스');
+    }, 1001);
 
     jest.runAllTimers();
   });
